3e15fbd2b1b8636a686d86a33ffca2de3e439445
hvcsc <jrhcmll13@gmail.com>
2025-05-27 23:15:19 +0800

    assign imported files

--- quiz_creator_oop/main_quiz_creator.py
+++ quiz_creator_oop/main_quiz_creator.py
@@ -3,5 +3,9 @@ from question_builder import QuestionBuilder
 from quiz_manager import QuizManager
 
 #define imported files
+def main():
+    builder = QuestionBuilder()
+    quiz = QuizManager()
+    
 #ask if the user wants to add more question
 #save and exit
