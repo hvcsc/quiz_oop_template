cfc4fd0af163901004955855ac2df7d5c4724c00
hvcsc <jrhcmll13@gmail.com>
2025-05-27 23:08:27 +0800

    add class and define the text file

--- quiz_creator_oop/quiz_manager.py
+++ quiz_creator_oop/quiz_manager.py
@@ -1,4 +1,9 @@
 #add class
-#define the text file
+class QuizManager:
+    #define the text file
+    def __init__(self, filename = "quiz_key.txt"):
+        self.filename = filename
+        self.questions = []
+
 #append questions
 #save inputs to file
