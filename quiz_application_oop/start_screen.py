2da4f28a44c5bd91ab9349847da27d98263c0942
hvcsc <jrhcmll13@gmail.com>
2025-05-28 00:43:00 +0800

    import tkinter and add class

--- quiz_application_oop/start_screen.py
+++ quiz_application_oop/start_screen.py
@@ -1,5 +1,12 @@
 #import tkinter
+import tkinter as tk
+
 #add class
-#assign
+class StartScreen:
+    def __init__(self, root, start_callback):
+        #assign
+        self.root = root
+        self.start_callback = start_callback
+
 #define screen, title label, start button
 #clear screen
