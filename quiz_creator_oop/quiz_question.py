900ab5f0df0ea417df9a0de44ea21fa7b58eb88d
hvcsc <jrhcmll13@gmail.com>
2025-05-27 22:50:30 +0800

    class for dictionaries and lists

--- quiz_creator_oop/quiz_question.py
+++ quiz_creator_oop/quiz_question.py
@@ -1,2 +1,7 @@
 #add class
+class Question:
 #define dictionaries and lists
+    def __init__(self, text, choices, answer):
+        self.text = text
+        self.choices = choices
+        self.answer = answer
