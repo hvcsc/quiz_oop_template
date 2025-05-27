922fae46574dcddfd8f385163c2fb60273cd0abb
hvcsc <jrhcmll13@gmail.com>
2025-05-28 00:51:31 +0800

    pseudocode

---
+++ quiz_application_oop/question_display.py
@@ -0,0 +1,8 @@
+#import tkinter
+#add class
+#define and assign
+#display of questions and options
+#control buttons on the bottom
+#check answer
+#permission to proceed to the next question
+#clear screen
