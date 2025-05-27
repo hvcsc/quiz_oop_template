6e708c64a84720033cfee699688b9fc457f734d4
hvcsc <jrhcmll13@gmail.com>
2025-05-28 02:35:17 +0800

    import tkinter, add class, define, and assign

--- quiz_application_oop/score_screen.py
+++ quiz_application_oop/score_screen.py
@@ -1,5 +1,15 @@
 #import tkinter
+import tkinter as tk
+
 #add class
-#define and assign
+class ScoreScreen:
+    #define and assign
+    def __init__(self, root, score, total_questions, restart_callback, exit_callback):
+        self.root = root
+        self.score = score
+        self.total_questions = total_questions
+        self.restart_callback = restart_callback
+        self.exit_callback = exit_callback
+        
 #congratulatory message, score, restart, and exit button display
-#clear screen 
+#clear screen
