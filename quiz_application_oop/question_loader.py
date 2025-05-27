5e099de81b4dad36127777a6d8063ad884c87b2e
hvcsc <jrhcmll13@gmail.com>
2025-05-28 03:16:33 +0800

    fixed path for the text file

--- quiz_application_oop/question_loader.py
+++ quiz_application_oop/question_loader.py
@@ -1,17 +1,20 @@
 #import package
+import os
 from tkinter import messagebox
 
 #add class
 class QuestionLoader:
-    def __init__(self, filename = "quiz_key.txt"):
+    def __init__(self):
         #assign file
-        self.filename = filename
+        self.questions = []
 
 #load questions from the text file
     def load_question(self):
-        questions = []
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(script_dir, "../quiz_creator_oop/quiz_key.txt")
+
         try:
-            with open(self.filename, "r") as file:
+            with open(file_path, "r") as file:
                 content = file.read().strip().split("\n\n")
                 for block in content:
                     lines = block.strip().split("\n")
@@ -23,11 +26,11 @@ class QuestionLoader:
                         'd': lines[4][3:].strip()
                     }
                     answer = lines[5].split(":")[1].strip()
-                    questions.append({
+                    self.questions.append({
                         "question": question,
                         "choices": choices,
                         "answer": answer
                     })
         except FileNotFoundError:
             messagebox.showerror("Error", f"{self.filename} not found!")
-        return questions
+        return self.questions
