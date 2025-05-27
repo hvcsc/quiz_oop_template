1564016587e4601cdb3463bd026558e025e120c6
hvcsc <jrhcmll13@gmail.com>
2025-05-28 02:44:15 +0800

    import files

--- quiz_application_oop/quiz_game.py
+++ quiz_application_oop/quiz_game.py
@@ -1,4 +1,13 @@
 #import files
+import tkinter as tk
+import random
+
+from question_loader import QuestionLoader
+from start_screen import StartScreen
+from question_display import QuestionsDisplay
+from pause_screen import PauseScreen
+from score_screen import ScoreScreen
+
 #add class
 #assign variables
 #start quiz function
