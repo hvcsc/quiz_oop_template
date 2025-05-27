6e53fe082d80dfb647d6462f552d222acbe7550e
hvcsc <jrhcmll13@gmail.com>
2025-05-27 23:32:31 +0800

    fixed indents

--- quiz_creator_oop/quiz_manager.py
+++ quiz_creator_oop/quiz_manager.py
@@ -6,18 +6,18 @@ class QuizManager:
         self.questions = []
 
         #append questions
-        def add_question(self, question):
-            self.questions.append(question)
+    def add_question(self, question):
+        self.questions.append(question)
 
-        #save inputs to file
-        def save_to_file(self):
-            with open(self.filename, "a") as file:
-                for q in self.questions:
-                    file.write(f"{q.text}\n")
-                    file.write(f"a. {q.choices['a']}\n")
-                    file.write(f"b. {q.choices['b']}\n")
-                    file.write(f"c. {q.choices['c']}\n")
-                    file.write(f"d. {q.choices['d']}\n")
-                    file.write(f"answer: {q.answer}\n")
-                    file.write("\n")
-            print(f"\n{len(self.questions)} question/s saved to '{self.filename}'.")
+    #save inputs to file
+    def save_to_file(self):
+        with open(self.filename, "a") as file:
+            for q in self.questions:
+                file.write(f"{q.text}\n")
+                file.write(f"a. {q.choices['a']}\n")
+                file.write(f"b. {q.choices['b']}\n")
+                file.write(f"c. {q.choices['c']}\n")
+                file.write(f"d. {q.choices['d']}\n")
+                file.write(f"answer: {q.answer}\n")
+                file.write("\n")
+        print(f"\n{len(self.questions)} question/s saved to '{self.filename}'.")
