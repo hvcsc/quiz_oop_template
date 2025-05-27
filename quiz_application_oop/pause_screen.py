06e9006561e761de6bd148ebb706a3c34a7f8cc0
hvcsc <jrhcmll13@gmail.com>
2025-05-28 02:28:57 +0800

    import tkinter, add class, and assign

--- quiz_application_oop/pause_screen.py
+++ quiz_application_oop/pause_screen.py
@@ -1,5 +1,12 @@
 #import tkinter
+import tkinter as tk
+
 #add class
-#defind and assign
+class PauseScreen:
+    #defind and assign
+    def __init__(self, root, resume_callback):
+        self.root = root
+        self.resume_callback = resume_callback
+
 #pause and resume button
 #clear screen
