12347c9ebe5f021f4119d2d541058ba01183b051
hvcsc <jrhcmll13@gmail.com>
2025-05-27 23:00:35 +0800

    input questions

--- quiz_creator_oop/question_builder.py
+++ quiz_creator_oop/question_builder.py
@@ -2,7 +2,12 @@
 from quiz_question import Question
 
 #add class
-#create questions
+class QuestionBuilder:
+    #create questions
+    def create_question(self):
+        print("\nEnter a question.")
+        text = input("Question: ")
+
 #add the choices
 #check if all choices are filled
 #input the correct answer
