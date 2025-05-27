66c33de4b4d6e8dd089b2f5fce59574a44acea3f
hvcsc <jrhcmll13@gmail.com>
2025-05-28 02:30:41 +0800

    pause and resume button display

--- quiz_application_oop/pause_screen.py
+++ quiz_application_oop/pause_screen.py
@@ -3,10 +3,31 @@ import tkinter as tk
 
 #add class
 class PauseScreen:
-    #defind and assign
+    #define and assign
     def __init__(self, root, resume_callback):
         self.root = root
         self.resume_callback = resume_callback
 
-#pause and resume button
+    #pause and resume button
+    def show(self):
+        self.clear_screen()
+        pause_label = tk.Label(
+            self.root,
+            text = "Quiz Paused",
+            font = ("Times New Roman", 20),
+            bg = "#EAE0D5",
+            fg = "#22333B"
+        )
+        pause_label.place(relx = 0.5, rely = 0.4, anchor = "center")
+
+        resume_button = tk.Button(
+            self.root,
+            text = "Resume",
+            font = ("Times New Roman", 14),
+            bg = "#C6AC8E",
+            fg = "#0A0908",
+            command = self.resume_callback
+        )
+        resume_button.place(relx = 0.5, rely = 0.6, anchor = "center")
+
 #clear screen
