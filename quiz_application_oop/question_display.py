aa627f1c29a05b79e934fdec2bf39740abb2e650
hvcsc <jrhcmll13@gmail.com>
2025-05-28 01:19:09 +0800

    import tkinter, define and assign

--- quiz_application_oop/question_display.py
+++ quiz_application_oop/question_display.py
@@ -1,6 +1,22 @@
 #import tkinter
+import tkinter as tk
+
 #add class
-#define and assign
+class QuestionsDisplay:
+    #define and assign
+    def __init__(self, root, questions, on_answer_checked, on_next, on_pause, on_exit):
+        self.root = root
+        self.questions = questions
+        self.question_index = 0
+        self.score = 0
+        self.is_paused = False
+
+        self.on_answer_checked = on_answer_checked
+        self.on_next = on_next
+        self.on_pause = on_pause
+        self.on_exit = on_exit
+
+        self.answer_buttons = []
 #display of questions and options
 #control buttons on the bottom
 #check answer
